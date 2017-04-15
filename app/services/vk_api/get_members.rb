e3f7bd541c298d3af19f473b57a28f9c863b5ba9
Kirill <kirill.fedyanin@gmail.com>
2017-04-15 20:27:10 +0300

    Download all

--- app/services/vk_api/get_members.rb
+++ app/services/vk_api/get_members.rb
@@ -1,21 +1,33 @@
 module VkApi
   class GetMembers < Base
-    # VkApi::GetMembers.new(10).info
     def initialize(group_id)
       @group_id = group_id
     end
 
     def info
-      @info ||= make_request['items']
+      result = make_request
+      if result['count'].to_i < 1000
+        result['items']
+      else
+        users = result['items']
+        count = result['count'].to_i
+        ((count - 1000)/1000.0).ceil.times do |offset|
+          @offset = (offset + 1) * 1000
+          users += make_request['items']
+        end
+        users
+      end
     end
 
     private
 
-    def params
+    def params()
       {
         group_id: @group_id,
+        offset: @offset || 0,
         fields: [
-          'bdate', 'city', 'country', 'universities', 'occupation', 'contacts'
+          'bdate', 'city', 'country', 'universities', 'occupation', 'contacts',
+          'about'
         ],
         access_token: APP_TOKEN,
         v: VERSION
